bf492b247bee73c3691b6a84f9f4cd95448ca0f9
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-06-10 21:00:37 +0900

    :zap: Add: health_checksコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :blogs
+  resources :blogs, only: %i[index show create]
 
   get '/health_check', to: 'health_checks#index'
 end
